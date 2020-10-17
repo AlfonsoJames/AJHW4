4918c999dce194e5ad456f22c52add15675447d9
Alfonso James <AlfonsoJames@users.noreply.github.com>
2020-10-17 11:03:56 -0400

    question 1 generated

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ function start() {
 // Above creates intial "Start Quiz" html
 // Below starts w/ question 1 starts global timer and sets 1st of conditions to end and prompt for name
     $(button).click(function(){
+        $("#buttons").empty();
         q1();
+        // $("#question").empty();
         console.log("Start Timer");     
             var timeleft = 4;
             var timer = setInterval(function(){
@@ -27,28 +29,25 @@ function start() {
     });
 }
 
-// Below is suppossed to generate contents of question 1 and lead to question two...still broken
-function q1() {
+// Generate contents of question 1 
     
     console.log("q1");
 
     var answers =[
-        "The Correct answer",
+        "The Close answer",
         "The completely wrong answer",
         "The misleading wrong answer"];
 
-    $("#buttons").empty();
-    $("#question").empty();
     $("#question").html("What is the answer to the first Question?");
+    
+    answers.forEach(function(answer) {
+        var button = $("<button>");
+        $(button).html(answer);   
+        var ul = $("#buttons");
+        $ (ul).append(button);
+        var correct = $("#correct");
+    });
+    // now I cant figure out how to separte the correct answer from the wrong answer:()
+    
 
-    var a1 = function() {
-        
-        var button = $("<li>");
-        $(button).html();
-        var ul = $("#ul");
-        $ (ul).append(button)
-        console.log("added button");
-    }
-    answers.forEach(a1);
-}
  
